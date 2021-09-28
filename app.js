4f32c254214a92e5a9b3610f5be38f7ae59109e4
zarose0000ds <zarose0000ds@gmail.com>
2021-09-28 17:40:51 +0800

    feat: import basic html and css files for index and show page

--- app.js
+++ app.js
@@ -9,9 +9,16 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: "main", extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// STATIC FILES
+app.use(express.static('public'))
+
 // ROUTES
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/restaurants/:id', (req, res) => {
+  res.render('show')
 })
 
 // LISTENING
