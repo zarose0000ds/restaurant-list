8bde8692baf39efc959e420438f3704a6f551a19
zarose0000ds <zarose0000ds@gmail.com>
2021-10-21 23:37:31 +0800

    feat: add facebook login feature

--- config/passport.js
+++ config/passport.js
@@ -26,6 +26,26 @@ module.exports = app => {
     }).catch(e => done(e, false))
   }))
 
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+    const { name, email } = profile._json
+
+    User.findOne({ email }).then(user => {
+      if (user) return done(null, user)
+
+      const randomPassword = Math.random().toString(36).slice(-8)
+      bcrypt.genSalt(10).then(salt => bcrypt.hash(randomPassword, salt)).then(hash => User.create({
+        name,
+        email,
+        password: hash
+      })).then(user => done(null, user)).catch(e => done(e, false))
+    })
+  }))
+
   // SERIALIZE
   passport.serializeUser((user, done) => {
     done(null, user.id)
