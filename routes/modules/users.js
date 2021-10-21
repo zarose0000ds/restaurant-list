e0dee38205de2de57ec2ad7fe85d8cc0d2c0ac2c
zarose0000ds <zarose0000ds@gmail.com>
2021-10-21 13:12:22 +0800

    fead: add login and register routes

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,12 +1,66 @@
 const express = require('express')
+const passport = require('passport')
+const bcrypt = require('bcrypt')
 const router = express.Router()
+
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login',
+  failureFlash: true}), (req, res) => {
+})
+
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.pish({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+
+  User.findOne({ email }).then(user => {
+    if (user) {
+      errors.push({ message: '這個 Email 已經註冊過了。' })
+      return res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt.genSalt(10).then(salt => bcrypt.hash(password, salt)).then(hash => User.create({
+      name,
+      email,
+      password: hash
+    })).then(() => res.redirect('/')).catch(e => console.log(e))
+  })
+})
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
 })
 
 module.exports = router
